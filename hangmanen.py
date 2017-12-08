59605912e68995c2d023cd2a70dca04801b664f8
Sacules <31902745+Sacules@users.noreply.github.com>
2017-12-08 15:50:00 -0300

    Finally works!
    The core of it is fine and working.

--- hangmanen.py
+++ hangmanen.py
@@ -24,9 +24,10 @@ class Hangmanen():
             for line in file:
                 # Delete any extra space at the end
                 line = line.strip()
-                
+                line = line.split(" - ", maxsplit = 1)
+
                 # Separates artist and song name
-                self.names.append(line.split(" - ", maxsplit = 1))
+                self.names.append(line)
                 
         print(self.names) # Just for testing
         #for i in self.names:
@@ -44,18 +45,18 @@ class Hangmanen():
             
             for item in name:
                 
-                new_item = ""
+                new_item = []
                 
                 for letter in item:
                     
                     if letter == " ":
-                        new_item = new_item + " "
+                        new_item = new_item + [" "]
                     
                     elif letter == "-":
-                        new_item = new_item + "-"
+                        new_item = new_item + ["-"]
                         
                     else:
-                        new_item = new_item + "_"
+                        new_item = new_item + ["_"]
                 
                 new_name.append(new_item)
             
@@ -95,27 +96,105 @@ class Hangmanen():
         
         return choice
     
-    def isThereWord(self, word):
+    
+    def replaceInList(self, wordOrLetter):
         
-        """Checks if the word guessed is in the list."""
+        """Checks if the word or letter guessed is in the list. Then replaces
+        it in the blank list."""
         
+        # Initial name
+        name_pos = 0
+        
+        # Will remain this if the word or letter isn't found
+        noHangman = 0
+
         for name in self.names:
+            
+            # First check on band name
+            item_pos = 0
             
             for item in name:
                 
-                if word in item:
-                    return True
+                char_pos = 0
+                tail = 0
+                
+                if wordOrLetter in item:
+                    
+                    # No body part gets drawn
+                    noHangman = 1
+                    
+                    # Letter
+                    if len(wordOrLetter) == 1:
+                        
+                        # Iterates until the whole item is replaced in blank
+                        while tail <= len(item):
+                            
+                            # Saves position
+                            char_pos = item.find(wordOrLetter, tail)
+                            
+                            # If there is no letter in the substring
+                            if char_pos == -1:
+                                break
+                            
+                            # Replace it
+                            self.blank_names[name_pos][item_pos][char_pos] = wordOrLetter
+                            
+                            # Take the substring after the first letter it finds
+                            tail = char_pos + 1
+                        
+                    
+                    # Word
+                    else:
+                        
+                        # Get position
+                        word_pos = item.find(wordOrLetter)
+                        
+                        for letter in wordOrLetter:
+                            
+                            # Replace one letter
+                            self.blank_names[name_pos][item_pos][word_pos] = letter
+                            
+                            # Move on to the next one
+                            word_pos += 1
+                        
+
+                # Switch to song name
+                item_pos = 1
+
+            # Switch to next name
+            name_pos += 1
         
-        return False
+        return noHangman
+    
+    
+    def printBlankList(self):
+        
+        """Kinda obvious isn't it?"""
+        
+        for name in self.blank_names:
 
+            item_pos = 0
 
+            for item in name:
 
-
+                # Prints the word
+                for letter in item:
+                    print(letter, end="")
+                
+                # After printing the artist, print a bar
+                if item_pos == 0:
+                    print(" - ", end="")
+                    item_pos += 1
+            
+            # New line after each song
+            print()
+    
 
 # Testing
 drunk = Hangmanen()
 drunk.loadNames()
 drunk.createBlankNames()
 print(drunk.checkComplete())
-drunk.askWordOrLetter()
-drunk.isThereWord("earth day")
+#drunk.askWordOrLetter()
+drunk.replaceInList("a")
+drunk.printBlankList()
