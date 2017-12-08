d328fed363d714d54fedfd227707a20992c76fde
Sacules <31902745+Sacules@users.noreply.github.com>
2017-12-08 02:19:10 -0300

    spaces

--- hangmanen.py
+++ hangmanen.py
@@ -7,24 +7,31 @@ class Hangmanen():
         self.names = []
         self.blank_names = []
     
+    
     def askForNames(self):
         
         """Prompts the user to enter the name of the list containing the names."""
         
         pass
     
+    
     def loadNames(self):
         
         """Loads the list of names from a text file, one per line."""
         
         with open(self.list_name, "r", encoding='utf-8') as file:
+            
             for line in file:
-                line = line.strip() # Deletes any extra space at the end
-                self.names.append(line.split(" - ", maxsplit = 1)) # Separates artist and song name
+                # Delete any extra space at the end
+                line = line.strip()
                 
-        print(self.names)
+                # Separates artist and song name
+                self.names.append(line.split(" - ", maxsplit = 1))
+                
+        print(self.names) # Just for testing
         #for i in self.names:
             #print(i)
+    
     
     def createBlankNames(self):
         
@@ -32,12 +39,15 @@ class Hangmanen():
         guess. They're saved in a text file."""
         
         for name in self.names:
+            
             new_name = []
             
             for item in name:
+                
                 new_item = ""
                 
                 for letter in item:
+                    
                     if letter == " ":
                         new_item = new_item + " "
                     
@@ -50,14 +60,53 @@ class Hangmanen():
                 new_name.append(new_item)
             
             self.blank_names.append(new_name)
-            
- 
+
+
     def checkComplete(self):
-        pass
-    
         
+        """Checks if all the letters or words have been guessed."""
+        
+        for name in self.blank_names:
+            
+            for item in name:
+                
+                if "_" in item:
+                    return False
+        
+        return True
 
 
+    def askWordOrLetter(self):
+        
+        """Prompts the user to choose what to guess."""
+        
+        while True:
+            print("Choose one: \n",
+                  "1. Guess a word. \n",
+                  "2. Guess a leter. \n")
+            
+            choice = int(input())
+            
+            if choice != 1 or choice != 2:
+                print("Error, insert a valid number.")
+
+            else:
+                break
+        
+        return choice
+    
+    def isThereWord(self, word):
+        
+        """Checks if the word guessed is in the list."""
+        
+        for name in self.names:
+            
+            for item in name:
+                
+                if word in item:
+                    return True
+        
+        return False
 
 
 
@@ -67,6 +116,6 @@ class Hangmanen():
 drunk = Hangmanen()
 drunk.loadNames()
 drunk.createBlankNames()
-                
-                
-                
+print(drunk.checkComplete())
+drunk.askWordOrLetter()
+drunk.isThereWord("earth day")
